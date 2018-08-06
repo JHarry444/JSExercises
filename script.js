0af7ba85f8691c10b92e45f89f7d08e9326f8b30
jharry444 <jordan.harrison@academytrainee.com>
2018-08-06 16:21:09 +0100

    Ex.5

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function square(input) {
 function sum(num1, num2, num3) {
     return num1 + num2 + num3;
 }
+
+function testSumSquare() {
+    window.alert("Square: " + square(2) + " Sum: " + sum(1, 2, 3));
+}
+
+function createPerson(name, age, occupation) {
+    var person = new Object();
+    person.name = name;
+    person.age = age;
+    person.occupation = occupation;
+    return person;
+}
