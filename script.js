f8660151947d7571b2c587fbb8846d2499cef3c1
jharry444 <jordan.harrison@academytrainee.com>
2018-08-06 16:29:02 +0100

    Ex.6

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ function sum(num1, num2, num3) {
 function testSumSquare() {
     window.alert("Square: " + square(2) + " Sum: " + sum(1, 2, 3));
 }
-
+var person;
 function createPerson(name, age, occupation) {
-    var person = new Object();
+    person = new Object();
     person.name = name;
     person.age = age;
     person.occupation = occupation;
-    return person;
+}
+
+function testPerson() {
+        createPerson("test", 44, "Unemployed");
+        alert("Name: " + person.name + " Age: " + person.age + " Occupation: " + person.occupation);
+        person.age = 99;
+        alert("Name: " + person.name + " Age: " + person.age + " Occupation: " + person.occupation);
+}
+
+function agePerson() {
+    person.age = person.age + 1;
+    alert("Name: " + person.name + " Age: " + person.age + " Occupation: " + person.occupation);
 }
