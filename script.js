cc93f17a54f3d9fd706968255b8881c06aa5e8a3
JHarry444 <jordan.harrison@academytrainee.com>
2018-08-06 17:03:04 +0100

    Ex. 6

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ function agePerson() {
     person.age = person.age + 1;
     alert("Name: " + person.name + " Age: " + person.age + " Occupation: " + person.occupation);
 }
+
+function createPersonFromFields() {
+    person = new Object();
+    editPerson();
+    document.getElementById("output").style.visibility = "visible";
+    document.getElementById("edit").style.visibility = "visible";
+}
+
+function outputPerson() {
+    alert("Name: " + person.name + " Age: " + person.age + " Occupation: " + person.occupation);
+}
+
+function editPerson() {
+    person.name = document.getElementById("name").value;
+    person.age = document.getElementById("age").value;
+    person.occupation = document.getElementById("occupation").value;
+}
